cca1b32ee7e945666bccc4f3bf7cbf3fd9fd8f6e
Omar <umar_317@hotmail.com>
2019-01-26 10:20:37 +0800

    v0.2 - decrease font size and add am and pm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,17 @@
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
+$(document).ready(function () {
+    let subuh = "05:00";
+    let zuhur = "12:00";
+    let asar = "03:00";
+    let maghrib = "04:00";
+    let isya = "07:00";
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+    $('#subuh').text("subuh : " + subuh + " am");
+    $('#zuhur').text("zuhur : " + zuhur + " pm");
+    $('#asar').text("asar : " + asar + " pm");
+    $('#maghrib').text("maghrib : " + maghrib + " pm");
+    $('#isya').text("isya : " + isya + " pm");
+});
